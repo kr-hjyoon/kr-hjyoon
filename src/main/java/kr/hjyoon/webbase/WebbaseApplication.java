6fbedd30d6a20f490edcd97425aa298acd27863a
hjyoon <kr-hjyoon@gmailcom>
2022-05-20 13:44:30 +0900

    최초등록

---
+++ src/main/java/kr/hjyoon/webbase/WebbaseApplication.java
@@ -0,0 +1,13 @@
+package kr.hjyoon.webbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/hjyoon/webbase/WebbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.hjyoon.webbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
